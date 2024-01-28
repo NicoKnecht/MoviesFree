32bb9167d8e0bd416e9dacfb731b54fa465d5616
nicoKnecht <nicosanmarco.dev@gmail.com>
2024-01-28 20:44:09 -0300

    add err

--- assets/request.js
+++ assets/request.js
@@ -5,9 +5,16 @@ const TOPRATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&la
 const UPCOMING = `https://api.themoviedb.org/3/movie/upcoming?api_key=${KEY}&language=en-US`;
 
 const getMovies = async (searchTerm, page = 1) => {// la respuesta es de a 20 peliculas suempre
-  const response = await fetch(`${searchTerm}&page=${page}`);// luego agrego termino de pagina en url e query param
+  try {
+    const response = await fetch(`${searchTerm}&page=${page}`);// luego agrego termino de pagina en url e query param
 
-  const data = await response.json();
-  console.log('data ==> ', data);
-  return data;
+    const data = await response.json();
+    console.log('data ==> ', data);
+
+    return data;
+
+  } catch (err) {
+    throw err;
+
+  }
 }
